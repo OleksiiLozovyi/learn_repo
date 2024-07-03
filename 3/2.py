5961d202a93295406870e7a513a2c7778074af91
LDLPROJECT\oleksii.lozovyi <oleksii.lozovyi@pragmaticplay.com>
2024-07-03 21:46:14 +0300

    HW3_1 update

--- 3/2.py
+++ 3/2.py
@@ -2,7 +2,11 @@ from datetime import datetime
 
 
 def get_days_from_today(date='2024-06-22'):
-    date = datetime.strptime(date, '%Y-%m-%d').date()
-    cur_date = datetime.today().date()
-    print((cur_date - date).days)
+    try:
+        date = datetime.strptime(date, '%Y-%m-%d').date()
+        cur_date = datetime.today().date()
+        return (cur_date - date).days
+    except:
+        print('Date must be YYYY-MM-DD')
 get_days_from_today()
+
