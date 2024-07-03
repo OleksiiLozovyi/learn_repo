9adb4cdf72f98ed1acc4814aab54bac84cad234b
LDLPROJECT\oleksii.lozovyi <oleksii.lozovyi@pragmaticplay.com>
2024-07-03 22:28:32 +0300

    HW3_2 update

--- 3/hw3_2.py
+++ 3/hw3_2.py
@@ -1,7 +1,12 @@
-from random import random, randint, choices
+from random import random, randint, choices, sample
 
-def get_numbers_ticket(min=1, max=49, quantity=6):
-    numbers = range(min,max)
-    chosen_item = sorted(choices(numbers, k=quantity))
-    print(chosen_item)
+def get_numbers_ticket(min=5, max=49, quantity=6):
+    try:
+        numbers = range(min,max)
+        chosen_item = sorted(sample(numbers, k=quantity))
+        return chosen_item
+    except:
+        chosen_item = []
+        print ('Min have to be < max')
+        return chosen_item
 get_numbers_ticket()
